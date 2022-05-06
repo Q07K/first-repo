3d3c75beec0297d7271a4c752d569137b771874d
Q07K <103545286+Q07K@users.noreply.github.com>
2022-05-06 13:34:54 +0900

    Update hell.py

--- hell.py
+++ hell.py
@@ -1,10 +1,10 @@
 for i in range(1, 21):
     if i % 15 == 0:
         a = 'yeardream'
-    elif i % 5 == 0:
-        a = 'dream'
     elif i % 3 == 0:
         a = 'year'
+    elif i % 5 == 0:
+        a = 'dream'
     else:
         a = i
     print(a)
